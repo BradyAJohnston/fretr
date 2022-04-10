02b832820f193817943d24319dda034b6c64b171
Brady Johnston <brady.johnston@me.com>
2022-04-10 11:29:03 +0800

    change acceptor argument to transfer, as it should be.

--- R/fret_calc.R
+++ R/fret_calc.R
@@ -1,14 +1,16 @@
 #' Calculate FRET from `donor` and `acceptor` signals.
 #'
-#' @param donor Numeric vector of donor signal.
-#' @param acceptor Numeric vector of acceptor signal.
+#' @param trans Signal from the transfer channel. (Excitation with donor laser
+#'   and emission in acceptor window.)
+#' @param donor Signal from the donor channel. (Excitation with the donor laser
+#'   and emission in donor window.)
 #'
 #' @return Numeric vector, the same length as donor and acceptor.
 #' @export
 #'
-fret_calc <- function(donor, acceptor) {
+fret_calc <- function(donor, trans) {
   stopifnot(is.numeric(donor))
-  stopifnot(is.numeric(acceptor))
+  stopifnot(is.numeric(trans))
 
-  acceptor / (donor + acceptor)
+  trans / (donor + trans)
 }
